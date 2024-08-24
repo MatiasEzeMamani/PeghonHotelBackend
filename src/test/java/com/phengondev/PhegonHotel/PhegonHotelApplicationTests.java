d0d705232827667449f18bfc6b9e9f0b00e627e9
Matias Mamani <matias.ezequiel.1177@gmail.com>
2024-08-24 05:30:09 -0300

    First Commit

---
+++ src/test/java/com/phengondev/PhegonHotel/PhegonHotelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.phengondev.PhegonHotel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhegonHotelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
