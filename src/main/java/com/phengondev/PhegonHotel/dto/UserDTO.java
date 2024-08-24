df012b36c316252ddd5d80de8bd23d5e7b8796f1
Matias Mamani <matias.ezequiel.1177@gmail.com>
2024-08-24 10:04:45 -0300

    Creation of models and DTO

---
+++ src/main/java/com/phengondev/PhegonHotel/dto/UserDTO.java
@@ -0,0 +1,21 @@
+package com.phengondev.PhegonHotel.dto;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.phengondev.PhegonHotel.model.Booking;
+
+import lombok.Data;
+
+@Data
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class UserDTO {
+	
+	private Long id;
+	private String email;
+	private String name;
+	private String phoneNumber;
+	private String role;
+	private List<BookingDTO> bookings = new ArrayList<>();
+}
---
+++ src/main/java/com/phengondev/PhegonHotel/model/Booking.java
@@ -0,0 +1,73 @@
+package com.phengondev.PhegonHotel.model;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name="bookings")
+public class Booking {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotNull(message = "Check-in date is required")
+	private LocalDate checkInDate;
+	
+	@NotNull(message = "Check-out date is required")
+	private LocalDate checkOutDate;
+	
+	@Min(value = 1, message = "Number of adults must not be less that 1")
+	private int numOfAdults;
+	
+	@Min(value = 0, message = "Number of children must not be less that 0")
+	private int numOfChildren;
+	private int totalNumOfGuest;
+	private String bookingConfirmationCode;
+	
+	@ManyToOne(fetch = FetchType.EAGER)
+	@JoinColumn(name = "user_id")
+	private User user;
+	
+	@ManyToOne(fetch = FetchType.EAGER)
+	@JoinColumn(name = "room_id")
+	private Room room;
+
+	public void calculatedTotalNumberOfGuest() {
+		this.totalNumOfGuest = this.numOfAdults + this.numOfChildren;
+	}
+
+	public void setNumOfAdults(int numOfAdults) {
+		this.numOfAdults = numOfAdults;
+		calculatedTotalNumberOfGuest();
+	}
+
+	public void setNumOfChildren(int numOfChildren) {
+		this.numOfChildren = numOfChildren;
+		calculatedTotalNumberOfGuest();
+	}
+
+	@Override
+	public String toString() {
+		return "Booking [id=" + id + ", checkInDate=" + checkInDate + ", checkOutDate=" + checkOutDate
+				+ ", numOfAdults=" + numOfAdults + ", numOfChildren=" + numOfChildren + ", totalNumOfGuest="
+				+ totalNumOfGuest + ", bookingConfirmationCode=" + bookingConfirmationCode + ", user=" + user
+				+ ", room=" + room + "]";
+	}
+	
+	
+	
+	
+}
